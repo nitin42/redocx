ba26c25bbbaa35253533fb25aa0099c8ba9feddc
Nitin <tulswani19@gmail.com>
2017-08-19 16:33:48 +0530

    open doc app after render

--- src/renderer/render.js
+++ src/renderer/render.js
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import os from 'os';
 import minimatch from 'minimatch';
 import execa from 'execa';
 import createElement from '../utils/createElement';
@@ -7,20 +8,27 @@ import { WordRenderer } from './renderer';
 import parse from './parse';
 
 /**
+ * Open the doc app
+ */
+function openDocApp(file) {
+	os.platform() === 'darwin' ? execa.shell(`open -a pages ${path.resolve(file)}`) : null;
+}
+
+/**
  * Document element
  * @param {Object} element 
  */
 function validateElement(element) {
-  if (element === undefined || element === null) {
-    throw new Error('Invalid component element.');
-  }
+	if (element === undefined || element === null) {
+		throw new Error('Invalid component element.');
+	}
 
-  if (typeof element === 'string') {
-    throw new Error(
-      'Invalid component element. Instead of passing string like \'text\', pass a class or functional component. For example - <Document />',
-    );
-  }
-  return true;
+	if (typeof element === 'string') {
+		throw new Error(
+			"Invalid component element. Instead of passing string like 'text', pass a class or functional component. For example - <Document />"
+		);
+	}
+	return true;
 }
 
 /**
@@ -28,17 +36,17 @@ function validateElement(element) {
  * @param {string} filePath 
  */
 function validatePath(filePath) {
-  if (filePath === null || filePath === undefined) {
-    throw new Error('Please specify a file path for the document');
-  }
+	if (filePath === null || filePath === undefined) {
+		throw new Error('Please specify a file path for the document');
+	}
 
-  const fileName = path.basename(filePath);
-  const pattern = '*.docx';
+	const fileName = path.basename(filePath);
+	const pattern = '*.docx';
 
-  if (!minimatch(fileName, pattern)) {
-    throw new Error(`Invalid filename '${path.basename(filePath)}'. Make sure the extension is '.docx'`);
-  }
-  return true;
+	if (!minimatch(fileName, pattern)) {
+		throw new Error(`Invalid filename '${path.basename(filePath)}'. Make sure the extension is '.docx'`);
+	}
+	return true;
 }
 
 /**
@@ -47,41 +55,41 @@ function validatePath(filePath) {
  * @param {string} filePath 
  */
 async function render(element, filePath) {
-  const container = createElement('ROOT');
+	const container = createElement('ROOT');
 
-  if (!validateElement(element)) {
-    return;
-  }
+	if (!validateElement(element)) {
+		return;
+	}
 
-  if (!validatePath(filePath)) {
-    return;
-  }
+	if (!validatePath(filePath)) {
+		return;
+	}
 
-  const node = WordRenderer.createContainer(container);
+	const node = WordRenderer.createContainer(container);
 
-  WordRenderer.updateContainer(element, node, null);
+	WordRenderer.updateContainer(element, node, null);
 
-  const output = await parse(container).toBuffer();
-  const stream = fs.createWriteStream(filePath);
+	const output = await parse(container).toBuffer();
+	const stream = fs.createWriteStream(filePath);
 
-  await new Promise((resolve) => {
-    output.doc.generate(stream);
-    console.log(`✨  Word document created at ${path.resolve(filePath)} !`);
-    execa.shell(`open -a pages ${path.resolve(filePath)}`);
-    resolve();
-  });
+	await new Promise(resolve => {
+		output.doc.generate(stream);
+		console.log(`✨  Word document created at ${path.resolve(filePath)} !`);
+    openDocApp(filePath);
+		resolve();
+	});
 }
 
 /**
  * Required for test the components
  */
 function testRenderer(element) {
-  const container = createElement('ROOT');
-  const node = WordRenderer.createContainer(container);
+	const container = createElement('ROOT');
+	const node = WordRenderer.createContainer(container);
 
-  WordRenderer.updateContainer(element, node, null);
+	WordRenderer.updateContainer(element, node, null);
 
-  return container;
+	return container;
 }
 
 export { render, testRenderer };
