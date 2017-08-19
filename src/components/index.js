d972d4b701cfcb325385860d33c15cba58c80686
Nitin <tulswani19@gmail.com>
2017-08-19 16:13:35 +0530

    init

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import BulletItem from './Bullet';
+import Document from './Document';
+import Footer from './Footer';
+import Header from './Header';
+import Hr from './Hr';
+import Image from './Image';
+import NumberItem from './Number';
+import LineBreak from './LineBreak';
+import List from './List';
+import PageBreak from './PageBreak';
+import Table from './Table';
+import Text from './Text';
+import WordDocument from './WordDocument';
+
+export {
+  BulletItem,
+  Document,
+  Footer,
+  Header,
+  Hr,
+  Image,
+  NumberItem,
+  LineBreak,
+  List,
+  PageBreak,
+  Table,
+  Text,
+  WordDocument,
+};
