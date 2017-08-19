dfb5eac3e0c6241681c9993330247b3bc5205742
Nitin <tulswani19@gmail.com>
2017-08-19 12:50:47 +0530

    demo

---
+++ demo/Word.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import TextComponent from '../examples/Text';
+import { render, Document } from '../src/';
+
+// Uncomment any of the below component to see what they render
+
+import FooterComponent from '../examples/Footer';
+import HeaderComponent from '../examples/Header';
+import HrComponent from '../examples/Hr';
+import LineBreakComponent from '../examples/LineBreak';
+import PageBreakComponent from '../examples/PageBreak';
+import TableComponent from '../examples/Table';
+import ListComponent from '../examples/List';
+import ImageComponent from '../examples/Image';
+
+class MyDocument extends Component {
+	render() {
+		return (
+			<Document>
+				<ListComponent />
+			</Document>
+		);
+	}
+}
+
+render(<MyDocument />, `${__dirname}/text.docx`);
