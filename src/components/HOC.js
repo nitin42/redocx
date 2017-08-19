869d2071f74b2e46fa68299ba1f73815f79908bd
Nitin <tulswani19@gmail.com>
2017-08-19 16:10:19 +0530

    removed unused import 'applyStyles'

---
+++ src/components/HOC.js
@@ -0,0 +1,39 @@
+import { renderNodes } from '../utils/nodes';
+
+/**
+ * High order component for rendering header and footer
+ * @param {string} component Component name
+ * @param {string} fn Function name
+ */
+function hoc(component, fn) {
+  class _Component {
+    children = [];
+
+    constructor(root, props) {
+      this.root = root;
+      this.props = props;
+      this.adder = fn === 'getHeader' ? this.root.doc.getHeader().createP() : this.root.doc.getFooter().createP();
+    }
+
+    appendChild(child) {
+      this.children.push(child);
+    }
+
+    removeChild(child) {
+      const index = this.children.indexOf(child);
+      this.children.slice(index, 1);
+    }
+
+    async renderChildren(align, styles) {
+      await renderNodes(align, this.props.align, styles, this.adder, this.children, this.props);
+    }
+
+    async render(align, styles) {
+      await this.renderChildren(align, styles);
+    }
+  }
+
+  return _Component;
+}
+
+export default hoc;
