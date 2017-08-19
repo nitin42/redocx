5346fa925a0ebf331cc19e0fcf08c763d4d098dd
Nitin <tulswani19@gmail.com>
2017-08-19 16:01:29 +0530

    disable no console

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "parser": "babel-eslint",
+    "rules": {
+        "no-await-in-loop": 0,
+        "no-param-reassign": 0,
+        "no-return-assign": 0,
+        "no-return-await": 0,
+        "no-console": 0,
+        "no-unneeded-ternary": 0
+    }
+};
