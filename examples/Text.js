1ef467ae6a260218e2fec5610798a2b38513ce9d
Nitin <tulswani19@gmail.com>
2017-08-19 15:45:24 +0530

    logo

--- examples/Text.js
+++ examples/Text.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import { Text, Document } from '../src/';
 
 class TextComponent extends Component {
-	render() {
-		return (
-			<Document>
-				<Text style={{ color: 'red', fontSize: '30' }} align="center">
-					Hello World!
-				</Text>
-			</Document>
-		);
-	}
+  render() {
+    return (
+      <Document>
+        <Text style={{ color: 'red', fontSize: '30' }} align="center">
+          Hello World!
+        </Text>
+      </Document>
+    );
+  }
 }
 
 export default TextComponent;
