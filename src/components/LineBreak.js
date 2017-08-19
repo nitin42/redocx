12bc203ff819777fc09737a0d0ee8d754f091dc1
Nitin <tulswani19@gmail.com>
2017-08-19 16:13:58 +0530

    create instance with name

---
+++ src/components/LineBreak.js
@@ -0,0 +1,12 @@
+/**
+ * This component adds a line break and should be called inside Text or List component
+ */
+class LineBreak {
+  constructor(root, props) {
+    this.root = root;
+    this.prop = props;
+    this.name = 'LineBreak';
+  }
+}
+
+export default LineBreak;
