aa0c61d82290b23a2fb3a98d23e874ac20bf33f7
Nitin <tulswani19@gmail.com>
2017-08-19 16:14:41 +0530

    unstable API

---
+++ src/components/Number.js
@@ -0,0 +1,33 @@
+import Root from './Root';
+import { renderNodes } from '../utils/nodes';
+
+/**
+ * Creates list of numbers (currently unstable)
+ */
+class NumberItem extends Root {
+  constructor(root, props) {
+    super(root, props);
+    this.root = root;
+    this.props = props;
+    this.adder = this.root.doc.createListOfNumbers();
+  }
+
+  appendChild(child) {
+    this.children.push(child);
+  }
+
+  removeChild(child) {
+    const index = this.children.indexOf(child);
+    this.children.slice(index, 1);
+  }
+
+  async renderChildren(align, styles) {
+    await renderNodes(align, null, styles, this.adder, this.children, this.props);
+  }
+
+  async render(align, styles) {
+    await this.renderChildren(align, styles);
+  }
+}
+
+export default NumberItem;
