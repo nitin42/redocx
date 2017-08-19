4f74e5d205266b5d4930a26daf9ace184f20ffe2
Nitin <tulswani19@gmail.com>
2017-08-19 16:15:29 +0530

    change class name 😆

---
+++ src/components/Root.js
@@ -0,0 +1,32 @@
+/**
+ * Base class that keeps track of parent and child nodes
+ */
+class Root {
+  parent = null;
+  children = [];
+
+  constructor(root, props) {
+    this.root = root;
+    this.props = props;
+  }
+
+  appendChild(child) {
+    child.parent = this;
+    this.children.push(child);
+  }
+
+  removeChild(child) {
+    const index = this.children.indexOf(child);
+
+    child.parent = null;
+    this.children.slice(index, 1);
+  }
+
+  async renderChildren() {
+    for (let i = 0; i < this.children.length; i += 1) {
+      await this.children[i].render();
+    }
+  }
+}
+
+export default Root;
