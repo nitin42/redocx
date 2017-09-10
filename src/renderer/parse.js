767465b0b5f2719ecd8a2bc6600bd94a366c540f
Nitin Tulswani <tulswani19@gmail.com>
2017-09-10 16:34:58 +0530

    added some comments

--- src/renderer/parse.js
+++ src/renderer/parse.js
@@ -5,10 +5,13 @@
  */
 const parse = (input) => {
   async function parseComponent(inputComponent) {
+    // This property is accessed due to https://github.com/nitin42/redocx/blob/master/src/renderer/renderer.js#L32  
     const document = inputComponent.document;
 
     await document.render();
-
+  
+    // Return the input component again because we rendered the children which weren't wrapped inside a parent.
+    // We async called the render method on all of the children.
     return inputComponent;
   }
 
